59eff084b7e6fb90a9e1830b4118b01ff05725a0
sindhud205 <sindhud5202@gmail.com>
2026-04-10 15:07:40 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,29 +3,43 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        // Automatically download and configure ChromeDriver
+        WebDriverManager.chromedriver().setup();
 
-        driver.get("https://automationexercise.com");
+        // Configure Chrome options
+        ChromeOptions options = new ChromeOptions();
 
-        driver.manage().window().maximize();
+        // Enable headless mode when running in Jenkins
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        if (headless) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
 
-        Thread.sleep(4000);
+        // Launch the browser
+        WebDriver driver = new ChromeDriver(options);
 
-        // Click first "Add to Cart" button
-        driver.findElement(By.xpath("(//a[contains(text(),'Add to cart')])[1]")).click();
+        try {
+            driver.manage().window().maximize();
+            driver.get("https://automationexercise.com");
 
-        Thread.sleep(4000);
+            // Explicit wait
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        // Click Continue Shopping
-        driver.findElement(By.xpath("//button[contains(text(),'Continue Shopping')]")).click();
-
-        System.out.println("\n\n\nFirst item added to cart successfully");
-
-        driver.quit();
-    }
-}
+            // Wait for and click the first "Add to Cart" button
+            By addToCartBtn = By.xpath("(//a[contains(text(),'Add to cart')])[1]");
+            wait.until(ExpectedConditions.elementToBeClickable(addToCartBtn)).
